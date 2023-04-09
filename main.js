b492099f60cf4baad7a2797311c3246d0297625b
maso22ru <maso22ru@student.ju.se>
2023-04-09 19:28:05 +0200

    java script logic

---
+++ main.js
@@ -0,0 +1,37 @@
+
+const tasks = []
+
+
+
+
+function addTask() {
+    const inputTextField = document.getElementById("text-add");
+
+    tasks.push(inputTextField.value);
+    console.log(tasks);
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+    const storageTasks = JSON.stringify(tasks);
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+    localStorage.setItem("tasks", storageTasks);
+
+    render(tasks);
+}
+
+function render(myTasks) {
+    const taskList = document.getElementById("tasks");
+    taskList.innerHTML = "";
+
+    // [1, 2, 3]
+    for (let i = 0; i < myTasks.length; i++) {
+        const li = document.createElement("li");
+        const itemValue = myTasks[i];
+        li.textContent = itemValue;
+    }
+}
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+const btn = document.getElementById("button-add");
+btn.addEventListener("click", addTask, false);
